ade6d076d7a053454d5c35bd2c7870bba1852252
shobhitatiwari22 <tiwaridurgacharan618@gmail.com>
2026-06-26 08:59:29 +0530

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 # 1. Page Configuration
 st.set_page_config(page_title="Interactive Heart Predictor", page_icon=":heart:", layout="centered")
 st.title("Live Heart Disease Risk Predictor")
-st.image("https://tinyurl.com/realhearticon", width=50)
+st.image("https://img.freepik.com/premium-photo/pretty-realistic-heart-illustration-with-isolated-background_742252-4140.jpg?w=2000", width=50)
 st.write("Move any slider or change any value below. The risk calculation updates instantly!")
 
 # 2. Load the Saved Model
